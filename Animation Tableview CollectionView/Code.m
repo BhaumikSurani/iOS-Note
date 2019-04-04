db8c1ee195d00a2a72399286ee276672f4824e5d
BhaumikSurani <34732349+BhaumikSurani@users.noreply.github.com>
2019-04-04 17:14:31 +0530

    Create Code.m

---
+++ Animation Tableview CollectionView/Code.m
@@ -0,0 +1,12 @@
+//Collapse and Expand With animation without Flinkring :-
+
+//For Collection View
+[collectionView performBatchUpdates:^{
+} completion:^(BOOL finished) {        
+}];
+
+
+//For Tableview
+[tableView performBatchUpdates:^{
+} completion:^(BOOL finished) {        
+}];
