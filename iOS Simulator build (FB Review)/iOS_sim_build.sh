cf2746171eef348b15c8be282d693d3e7a412250
BhaumikSurani <34732349+BhaumikSurani@users.noreply.github.com>
2020-02-26 13:09:40 +0530

    Create iOS_sim_build.sh

---
+++ iOS Simulator build (FB Review)/iOS_sim_build.sh
@@ -0,0 +1,3 @@
+ditto -ck --sequesterRsrc --keepParent `ls -1 -d -t ~/Library/Developer/Xcode/DerivedData/*/Build/Products/*-iphonesimulator/*.app | head -n 1` path/to/YourApp.zip
+
+## https://developers.facebook.com/docs/ios/getting-started/advanced
